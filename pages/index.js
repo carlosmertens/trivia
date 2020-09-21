874aa95a6b6bc146d1012d20c6875157fdd28ed4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-09-21 10:03:05 +0200

    setup data fetching functionality

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { PageLayout } from '../components/PageLayout';
 import { PageHeader } from '../components/PageHeader';
 import { ContentContainer } from '../components/ContentContainer';
@@ -10,6 +11,13 @@ export default function Home() {
       <ContentContainer>
         <ContentText />
       </ContentContainer>
+      <div className='flex items-center justify-center'>
+        <Link href='/quiz'>
+          <a className='w-48 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+            Play Now
+          </a>
+        </Link>
+      </div>
     </PageLayout>
   );
 }
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { PageLayout } from '../components/PageLayout';
 import { PageHeader } from '../components/PageHeader';
 
-export default function Home() {
+const Quiz = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const apiUrl =
+      'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
+    const fetchData = async () => {
+      const response = await axios.get(apiUrl);
+      setQuestions(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(questions);
+
   return (
     <PageLayout>
       <PageHeader>Category: Title</PageHeader>
     </PageLayout>
   );
-}
+};
+
+export default Quiz;
